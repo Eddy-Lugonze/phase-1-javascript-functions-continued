0b2e605313d6ecdc6eda1bf4d8451633e9eac8f3
Eddy-Lugonze <eddylugonze@gmail.com>
2025-03-13 01:49:28 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+// Function declaration for saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Function expression for mondayWork
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+// Function that returns another function (wrapAdjective)
+function wrapAdjective(symbol = "*") {
+    return function(adjective = "special") {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    };
+}
+
+// Exporting functions for testing
+module.exports = { saturdayFun, mondayWork, wrapAdjective };
